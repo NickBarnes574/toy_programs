952c4bd001ee475f7712c6fdc1399a7f15ab69b8
Nick Barnes <nick.a.barnes44@gmail.com>
2023-07-10 23:07:41 -0400

    create typewriter program

---
+++ 1_Typewriter/include/typewriter_functions.h
@@ -0,0 +1,13 @@
+/**
+ * @file typewriter_functions.h
+ *
+ * @brief Module for typewriter specific functions
+ */
+#ifndef _TYPEWRITER_FUNCTIONS_H
+#    define _TYPEWRITER_FUNCTIONS_H
+
+int typewriter(void);
+
+#endif /* _TYPEWRITER_FUNCTIONS_H */
+
+/*** end of file ***/
