d856ef6fae9a64498a0e96f114f57a342cc1ee2a
Nick Barnes <nick.a.barnes44@gmail.com>
2023-07-20 17:15:03 -0400

    add ability to keep window running until user closes it

--- 2_SDL/src/sdl_main.c
+++ 2_SDL/src/sdl_main.c
@@ -3,6 +3,7 @@
 
 #include "sdl_functions.h"
 #include "utilities.h"
+#include <stdbool.h>
 
 #define WINDOW_WIDTH  400
 #define WINDOW_HEIGHT 200
@@ -12,8 +13,10 @@ int main(int argc, char ** argv)
 {
     (void)argc;
     (void)argv;
-    int          exit_code = E_FAILURE;
-    SDL_Window * window    = NULL;
+    int          exit_code       = E_FAILURE;
+    SDL_Window * window          = NULL;
+    SDL_Event    event           = { 0 };
+    bool         program_running = true;
 
     SDL_Init(SDL_INIT_EVERYTHING);
 
@@ -32,7 +35,18 @@ int main(int argc, char ** argv)
         goto END;
     }
 
-    SDL_Delay(MS_DELAY);
+    while (program_running)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (SDL_QUIT == event.type)
+            {
+                program_running = false;
+                break;
+            }
+        }
+    }
+
     SDL_DestroyWindow(window);
     exit_code = E_SUCCESS;
 END:
